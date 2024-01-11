9c42f1893b82ef262d6b0ad5c063df6291afdae0
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2024-01-11 21:02:08 +0100

    add pseudo code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        // get informations in carte.txt
+        UseTextFile useTextFile = new UseTextFile();
+        String map = useTextFile.readFileText();
+
+        // Add all characteres bi dimensional array and Display map
+        // afficher tous les éléments dans arrayMaker
+        for (Character[] row : useTextFile.arrayMaker(map)) {
+            System.out.print(" ");
+            for (Character ch : row) {
+                System.out.print(ch);
+            }
+        }
     }
 }
+
+
+/*
+* 1) Ouvrir le fichier
+* 2) Parcourir le fichier ligne par ligne
+* 3) placer chque caractères issus de chque ligne du fichier dans un tableau
+*   -> tableau à 2 dimensions
+* 4) Afficher le tableau à 2 dimensions
+* 5) Demander à l'utilisateur de saisir des coordonnés de départ
+*   -> Si coordonnées impossibles ou incorrectes faire une boucle pour demander de nouvelles coordonnées
+*   -> Si coordonnées OK
+*       -> Demander les coordonnées d'arrivée
+*               -> Si coordonées impossibles ou incorrectes boucler pour demander d'autres coordonnées
+* 6) Demander les caps à suivre sous forme de 'char' : N S E O
+*   -> Chaque char correspond à une case
+*       -> Récupérer les caps sous forme de 'STR'   exemple : NNSSEESS
+*           -> Parcourir les caps 'char' par 'char'
+*           -> N = +1 / S = -1 / E= -1 / O= +1
+*
+*       Placer chaque cap dans une arraysList
+*           -> Pour chque 'char', appliquer la valeur de déplacement horizontale / verticale
+*               -> utilisation d'une boucle for pour appliquer les valeurs de déplacement
+*
+* Idée : mettre un caractère particulier pour matériler le point de départ et le point d'arrivée (si les coordonnées sont valides)
+*
+*
+*
+* */
--- src/UseTextFile.java
+++ src/UseTextFile.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class UseTextFile {
+import java.io.*;
+public class UseTextFile {
+
+    // Constructor by default
+    public UseTextFile(){
+
+
+    }
+
+    // method to read file text
+    protected String readFileText() throws IOException {
+        File map = new File("src/carte.txt");
+        BufferedReader br = new BufferedReader(new FileReader(map));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+
+        br.close();
+        return stringBuilder.toString();
+    }
+
+
+    // méthode pour récupérer chaque caractère de la string pour la placer dans un char dans un tableau à 2 dimensions
+    protected Character[][] arrayMaker(String readeDataTextFile){
+        Character[][] car = new Character[20][21];
+
+        int index = 0;
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 21; j++) {
+                if (index < readeDataTextFile.length()) {
+                    car[i][j] = readeDataTextFile.charAt(index++);
+                }
+            }
+        }
+
+        return car;
+    }
 }
