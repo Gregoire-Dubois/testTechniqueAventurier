67736b94940b7173911db1ad8d4a2afe856177ee
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2024-01-12 12:57:15 +0100

    add try catch in Game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
@@ -7,28 +8,26 @@ public class Game {
     }
 
     protected void testPositions(){
-        Scanner scannerXPositionStart = new Scanner(System.in);
-        System.out.println("Entrez la valeur du point de départ (X entre 0 à 19 inclus uniquement)");
-        System.out.println("Exemple : 2  -> ");
-        int inputX = scannerXPositionStart.nextInt();
 
-        Scanner scannerYPositionStart = new Scanner(System.in);
-        System.out.println("Entrez la valeur du point de départ (Y prévue entre 0 à 19 inclus uniquement)");
-        System.out.println("Exemple : 10  -> ");
-        int inputY = scannerXPositionStart.nextInt();
+        try{
+            Scanner scannerXPositionStart = new Scanner(System.in);
+            System.out.println("Entrez la valeur du point de départ (X entre 0 à 19 inclus uniquement)");
+            System.out.println("Exemple : 2  -> ");
+            int inputX = scannerXPositionStart.nextInt();
 
-        // vérifications on input position's datas
-        if (inputX >= 0 && inputX <= 19) {
-            System.out.println("ok");
-        } else {
-            System.out.println("valeurs numériques hors tableau");
+            Scanner scannerYPositionStart = new Scanner(System.in);
+            System.out.println("Entrez la valeur du point de départ (Y prévue entre 0 à 19 inclus uniquement)");
+            System.out.println("Exemple : 10  -> ");
+            int inputY = scannerYPositionStart.nextInt();
+
+            if (inputX >= 0 && inputX <= 19 && inputY >= 0 && inputY <= 19)  {
+                System.out.println("ok");
+            } else {
+                System.out.println("Les valeurs autorisées sont des nombres entiers entre 0 et 19 inclus");
+            }
+
+        }catch (InputMismatchException e){
+            System.out.println("merci d'indiquer des nombres entiers");
         }
-
-
-
     }
-
-
-
-
 }
